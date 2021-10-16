0dd4e89276dd79bd926631c69f5ce3a033c28b61
seherrr <86481287+seherrr@users.noreply.github.com>
2021-10-16 11:14:00 +0300

    Update HavaDurumu.js

--- src/components/HavaDurumu.js
+++ src/components/HavaDurumu.js
@@ -6,14 +6,13 @@ const HavaDurumu = (props) => {
             You need to allow location access...... < /p>;
     }
 
-    return ( <
-        div >
-        <
-        h3 > { weather.name } < /h3>  <
-        h4 > { weather.weather.map((data) => data.description).join(", ") } < /h4>  <
-        p > { weather.main.temp }℃ < /p>  <
-        p > { new Date(weather.dt * 1000).toLocaleDateString() } < /p>  <
-        /div>
+    return ( 
+        <div>
+            <h3> { weather.name } </h3> 
+            <h4> { weather.weather.map((data) => data.description).join(", ") } </h4> 
+            <p> { weather.main.temp }℃ </p> 
+            <p> { new Date(weather.dt * 1000).toLocaleDateString() } </p> 
+        </div>
     );
 };
 
